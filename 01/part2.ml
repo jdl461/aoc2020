0809ac3b268b8aa9924c8c5f17607aa43d07910c
jdl <confusionary@gmail.com>
2020-12-01 21:23:10 -0500

    Use n_cartesian_product function

--- 01/part2.ml
+++ 01/part2.ml
@@ -1,9 +1,18 @@
 open Core
 open Base
 
+let rec n_cartesian_product = function
+| [] -> [[]]
+| h :: t ->
+    let rest = n_cartesian_product t in
+    List.concat
+      (List.map ~f:(fun i -> List.map ~f:(fun r -> i :: r) rest) h)
+
 let entries = List.map (In_channel.read_lines "input1.txt") ~f:Int.of_string
 
-let () = 
-List.iter entries ~f:(fun (a) -> 
-  List.iter entries ~f:(fun (b) -> 
-    List.iter entries ~f:(fun (c) -> if (a+b+c) = 2020 then Stdio.print_endline (Int.to_string (a*b*c)))))
+let trips = n_cartesian_product [entries; entries; entries]
+let sums = List.map trips ~f:(fun (lst) -> (List.fold lst ~f:( + ) ~init:0, List.fold lst ~f:( * ) ~init:1))
+let () = let result = List.find sums ~f:(fun (a,_) -> (phys_equal a 2020)) in
+match result with
+  | Some (_,b) -> Stdio.print_endline (Int.to_string (b))
+  | None -> Stdio.print_endline "None"
