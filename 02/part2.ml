aad82542ba972a1477fc2f495d4986df725152d3
jdl <confusionary@gmail.com>
2020-12-02 23:47:40 -0500

    Use fancy monadic operators

--- 02/part2.ml
+++ 02/part2.ml
@@ -1,12 +1,14 @@
 open Core
 
+let (let*) o f = Option.bind o ~f:f
+
 let entries = In_channel.read_lines "input1.txt"
 
 let extract_value lst idx = match lst with
-[] -> ""
-| h :: _ -> Re2.Match.get_exn ~sub:(`Index idx) h 
+[] -> None
+| h :: _ -> Re2.Match.get ~sub:(`Index idx) h
 
-let xor a b = 
+let xor a b =
   match (a,b) with
   | true, true -> false
   | true, false -> true
@@ -17,16 +19,20 @@ let regex = Re2.create_exn "(\\d+)-(\\d+) ([a-z]): (.+)"
 let matches = List.map entries ~f:(fun entry -> 
   let result = Re2.get_matches regex entry in
   match result with
-    | Ok x -> let min = Int.of_string (extract_value x 1) - 1 in
-    let max = Int.of_string (extract_value x 2) - 1 in
-    let ch = extract_value x 3 in
-    let pwd = extract_value x 4 in
-    let minc = Char.to_string (String.get pwd min) in
-    let maxc = Char.to_string (String.get pwd max) in
-    xor 
-      ((String.compare minc ch) = 0)
-      ((String.compare maxc ch) = 0)
-    | Error _ -> false
+    | Ok x -> 
+      let* min = extract_value x 1 in
+      let* max = extract_value x 2 in
+      let* ch = extract_value x 3 in
+      let* pwd = extract_value x 4 in
+      let minc = Char.to_string (String.get pwd ((Int.of_string min) - 1)) in
+      let maxc = Char.to_string (String.get pwd ((Int.of_string max) - 1)) in
+      let matched = xor 
+        ((String.compare minc ch) = 0)
+        ((String.compare maxc ch) = 0) in
+      (match matched with
+      | true -> Some true
+      | false -> None)
+    | Error _ -> None
 )
 
-let () = Stdio.print_endline (Int.to_string (List.length (List.filter matches ~f:(fun m -> m))))
+let () = Stdio.print_endline (Int.to_string (List.length (List.filter matches ~f:Option.is_some)))
